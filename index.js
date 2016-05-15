e36f03c06667d97d76acc7c13e16fc5a107531f1
Tulir293 <tulir293@maunium.net>
2016-05-15 23:04:09 +0300

    Added missing * after /u/

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("sdk/page-mod").PageMod({
   contentScriptWhen: 'start',
-  include: ['file:///r/*', 'file:///u/'],
+  include: ['file:///r/*', 'file:///u/*'],
   onAttach: function(worker) {
     worker.tab.url = "https://reddit.com" + worker.tab.url.substring("file://".length);
   }
